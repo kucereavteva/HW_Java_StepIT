81f9957297fb66d544aed521284eae3734658bf3
kucereavteva <kucereavteva@icloud.com>
2022-09-08 02:10:00 +0300

    .deleted row

--- src/L4HW/HW4_5.java
+++ src/L4HW/HW4_5.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class HW4_5 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Vvedite cislo");
